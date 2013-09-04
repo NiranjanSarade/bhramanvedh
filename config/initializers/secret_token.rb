09ef0fb9c516a24606e95f4700a782e2519be88c
Niranjan Sarade <nirusuma@gmail.com>
2013-09-04 21:15:53 +0530

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bhramanvedh::Application.config.secret_key_base = 'e5add128504514e1fd040e21ea61309355fced957e3210f6cbdcc250757aedb049c07129157a776c51682a35d9552bc83147fb60da280bc224be45f55ddf5d78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bhramanvedh::Application.config.session_store :cookie_store, key: '_bhramanvedh_session'
